e973d1899fe9d1f179049a38991e75bb397d89c5
Monarul Islam <monarul007@gmail>
2025-11-18 15:39:12 +0600

    chore(routing): Remove unused admin controller imports from web routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\Admin\AdminModuleController;
-use App\Http\Controllers\Admin\AdminSettingsController;
 
 Route::get('/', function () {
     return view('welcome');
